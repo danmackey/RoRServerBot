f204b1d43dc387e532652c6926264fb9c36cdb5b
Danny Mackey <danmack768@gmail.com>
2023-09-02 19:54:42 -0400

    RoRClient now makes announcements and listens for commands

---
+++ ror_server_bot/ror_bot/commands.py
@@ -0,0 +1,250 @@
+from enum import auto, StrEnum
+from typing import Annotated, Literal, TYPE_CHECKING
+
+from pydantic import BaseModel, Field, RootModel
+
+from ror_server_bot import __version__
+
+from .enums import AuthStatus, Color, RoRClientEvents
+
+if TYPE_CHECKING:
+    from .ror_client import RoRClient
+
+
+COMMAND_PREFIX = '>'
+
+
+class CommandType(StrEnum):
+    PREFIX = auto()
+    PING = auto()
+    HELP = auto()
+    BRB = auto()
+    AFK = auto()
+    BACK = auto()
+    GTG = auto()
+    VERSION = auto()
+    COUNTDOWN = auto()
+
+
+class BaseChatCommand(BaseModel):
+    command: CommandType
+    args: list[str] = Field(default_factory=list)
+
+    @property
+    def description(self) -> str:
+        return 'No description provided.'
+
+    @property
+    def usage(self) -> str:
+        return 'No usage provided.'
+
+    @property
+    def name(self) -> str:
+        return self.command.value
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        """Execute the command.
+
+        :param client: The client to execute the command for.
+        :param uid: The UID of the user who sent the command.
+        """
+        raise NotImplementedError()
+
+
+class PrefixCommand(BaseChatCommand):
+    command: Literal[CommandType.PREFIX]
+
+    @property
+    def description(self) -> str:
+        return 'Get the prefix for commands.'
+
+    @property
+    def usage(self) -> str:
+        return f'{COMMAND_PREFIX}{self.name}'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        await client.send_chat(
+            f'The prefix for commands is: {COMMAND_PREFIX}'
+        )
+
+
+class PingCommand(BaseChatCommand):
+    command: Literal[CommandType.PING]
+
+    @property
+    def description(self) -> str:
+        return 'Ping the bot.'
+
+    @property
+    def usage(self) -> str:
+        return f'{COMMAND_PREFIX}{self.name}'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        await client.send_chat('pong')
+
+
+class HelpCommand(BaseChatCommand):
+    command: Literal[CommandType.HELP]
+
+    @property
+    def description(self) -> str:
+        return 'Get help for commands.'
+
+    @property
+    def usage(self) -> str:
+        return f'{COMMAND_PREFIX}{self.name} [command]'
+
+    def no_args(self) -> str:
+        commands = ', '.join(CommandType.__members__.values())
+        return (
+            f'Available commands: {commands}\n'
+            f'Use {self.usage} for more information.'
+        )
+
+    def one_arg(self) -> str:
+        cmd_name = self.args[0]
+        if cmd_name in CommandType.__members__.values():
+            command = chat_command_factory(cmd_name)
+            return (
+                f'{COMMAND_PREFIX}{self.name} {cmd_name}\n'
+                f'Description: {command.description}\n'
+                f'Usage: {command.usage}'
+            )
+        else:
+            return f'Invalid command {cmd_name}'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        if len(self.args) == 0:
+            message = self.no_args()
+        elif len(self.args) == 1:
+            message = self.one_arg()
+        else:
+            message = 'Too many arguments'
+
+        # why can't AUTH_STATUS.BOT use !say ???
+        if client.auth_status in AuthStatus.MOD | AuthStatus.ADMIN:
+            await client.say(uid, message)
+        else:
+            await client.send_chat(message)
+
+
+class SetStatusCommand(BaseChatCommand):
+    command: Literal[
+        CommandType.BRB,
+        CommandType.AFK,
+        CommandType.BACK,
+        CommandType.GTG
+    ]
+
+    @property
+    def description(self) -> str:
+        return f'Set your status to {self.name}.'
+
+    @property
+    def usage(self) -> str:
+        return f'{COMMAND_PREFIX}{self.name}'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        if len(self.args) == 0:
+            username = client.server.get_username_colored(uid)
+            if self.command is CommandType.BRB:
+                message = f'{username} will brb!'
+            elif self.command is CommandType.AFK:
+                message = f'{username} is afk!'
+            elif self.command is CommandType.BACK:
+                message = f'{username} is back!'
+            elif self.command is CommandType.GTG:
+                message = f'{username} is gtg'
+        else:
+            message = 'Too many arguments'
+
+        await client.send_chat(message)
+
+
+class GetVersionCommand(BaseChatCommand):
+    command: Literal[CommandType.VERSION]
+
+    @property
+    def description(self) -> str:
+        return 'Get the version of the bot.'
+
+    @property
+    def usage(self) -> str:
+        return f'{COMMAND_PREFIX}{self.name}'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        await client.send_chat(f'RoR Server Bot v{__version__}')
+
+
+class CountdownCommand(BaseChatCommand):
+    command: Literal[CommandType.COUNTDOWN]
+
+    @property
+    def description(self) -> str:
+        return 'Start a countdown.'
+
+    @property
+    def usage(self) -> str:
+        return f'{COMMAND_PREFIX}{self.name} <seconds>'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        if len(self.args) != 1:
+            await client.send_chat('Invalid number of arguments')
+            return
+
+        try:
+            seconds = int(self.args[0])
+        except ValueError:
+            await client.send_chat('Invalid argument')
+            return
+        else:
+            if seconds < 0:
+                await client.send_chat('Invalid argument')
+                return
+
+        username = client.server.get_username(uid)
+        await client.send_chat(
+            f'{username} started a {seconds} second countdown!'
+        )
+
+        # start at 1 to immediately send the first countdown message
+        time = 1
+
+        @client.server.on(RoRClientEvents.FRAME_STEP)
+        async def on_frame_step(delta: float):
+            # nonlocal is needed to modify the time and seconds variables
+            nonlocal time, seconds
+
+            time += delta
+
+            if time >= 1:
+                if seconds > 0:
+                    await client.send_chat(f'{Color.RED}\t{seconds}')
+                    seconds -= 1
+                    time = 0
+                else:
+                    await client.send_chat(f'{Color.GREEN}\tGO!!!')
+                    client.server.remove_listener(
+                        RoRClientEvents.FRAME_STEP,
+                        on_frame_step
+                    )
+
+
+ChatCommandTypes = (
+    PrefixCommand
+    | PingCommand
+    | HelpCommand
+    | SetStatusCommand
+    | GetVersionCommand
+    | CountdownCommand
+)
+
+ChatCommand = Annotated[ChatCommandTypes, Field(discriminator='command')]
+
+
+def chat_command_factory(message: str) -> ChatCommand:
+    command, *args = message.split(' ')
+    return RootModel[ChatCommand].model_validate({
+        'command': command,
+        'args': args
+    }).root
--- ror_server_bot/ror_bot/ror_client.py
+++ ror_server_bot/ror_bot/ror_client.py
@@ -1,11 +1,60 @@
 import asyncio
 import logging
-from typing import Callable
+import math
 
+from .commands import chat_command_factory, COMMAND_PREFIX
+from .enums import AuthStatus, RoRClientEvents
 from .models import RoRClientConfig
-from .ror_connection import RoRClientEvents, RoRConnection
+from .ror_connection import RoRConnection
 
 logger = logging.getLogger(__name__)
+
+
+class AnnouncementsHandler:
+    def __init__(self, delay: int, messages: list[str], color: str) -> None:
+        """Create a new AnnouncementsHandler.
+
+        :param delay: The delay between announcements in seconds.
+        :param messages: The messages to announce.
+        :param color: The color to use for the announcements.
+        """
+        self._delay = delay
+        self._messages = messages
+        self._color = color
+        self._time: float = 0
+        self._idx: int = 0
+
+    def try_next(self, delta: float) -> str | None:
+        """Try to get the next announcement. Returns None if no
+        announcement is ready. Otherwise, returns the announcement.
+
+        The announcement format is as follows:
+        ```
+        <color>ANNOUNCEMENT: <message>
+        ```
+        Where `<color>` is the hex color to use for the announcement and
+        `<message>` is the message to announce.
+
+        :param delta: The time since the last frame step.
+        :return: The announcement if one is ready, otherwise None.
+        """
+        self._time += delta
+
+        if self._time >= self._delay:
+            self._time = 0
+
+            idx = self._idx
+
+            if idx == len(self._messages) - 1:
+                self._idx = 0
+            else:
+                self._idx += 1
+
+            message = self._messages[idx]
+
+            return f'{self._color}ANNOUNCEMENT: {message}'
+        else:
+            return None
 
 
 class RoRClient:
@@ -14,35 +63,51 @@ class RoRClient:
 
         :param client_config: The configuration to use for the client.
         """
-        self.config = client_config
+        self._reconnection_tries = client_config.reconnection_tries
+        self._reconnection_interval = client_config.reconnection_interval
 
         self.server = RoRConnection(
-            username=self.config.user.name,
-            user_token=self.config.user.token,
-            password=self.config.server.password,
-            host=self.config.server.host,
-            port=self.config.server.port,
+            username=client_config.user.name,
+            user_token=client_config.user.token,
+            password=client_config.server.password,
+            host=client_config.server.host,
+            port=client_config.server.port,
         )
 
+        self._announcements_enabled = client_config.announcements.enabled
+        self._announcements_handler = AnnouncementsHandler(
+            delay=client_config.announcements.delay,
+            messages=client_config.announcements.messages,
+            color=client_config.announcements.color,
+        )
+
+        self.server.on(RoRClientEvents.FRAME_STEP, self._on_frame_step)
+        self.server.on(RoRClientEvents.CHAT, self._on_chat)
+
+    @property
+    def auth_status(self) -> AuthStatus:
+        """The authentication status of the client."""
+        return self.server.auth_status
+
     async def __aenter__(self):
-        for attempt in range(self.config.reconnection_tries):
+        for attempt in range(self._reconnection_tries):
             try:
                 logger.info(
                     'Attempt %d/%d to connect to RoR server: %s',
                     attempt + 1,
-                    self.config.reconnection_tries,
+                    self._reconnection_tries,
                     self.server.address
                 )
                 self.server = await self.server.__aenter__()
             except ConnectionRefusedError:
                 logger.warning('Connection refused!')
 
-                if attempt < self.config.reconnection_tries - 1:
+                if attempt < self._reconnection_tries - 1:
                     logger.info(
                         'Waiting %.2f seconds before next attempt',
-                        self.config.reconnection_interval
+                        self._reconnection_interval
                     )
-                    await asyncio.sleep(self.config.reconnection_interval)
+                    await asyncio.sleep(self._reconnection_interval)
             else:
                 break
 
@@ -51,7 +116,7 @@ class RoRClient:
         else:
             raise ConnectionError(
                 f'Could not connect to RoR server {self.server.address} '
-                f'after {self.config.reconnection_tries} attempts',
+                f'after {self._reconnection_tries} attempts',
             )
 
         return self
@@ -59,33 +124,92 @@ class RoRClient:
     async def __aexit__(self, exc_type, exc, tb):
         await self.server.__aexit__(exc_type, exc, tb)
 
-    def on(self, event: RoRClientEvents, listener: Callable | None = None):
-        """Decorator to register an event handler on the event emitter.
+    async def _on_frame_step(self, delta: float):
+        if self._announcements_enabled:
+            message = self._announcements_handler.try_next(delta)
 
-        Example:
-        ```
-        @client.on(RoRClientEvents.USER_JOIN)
-        def on_user_join(unique_id: int, user_info: UserInfo):
-            print(f'User {user_info.name} joined the server')
-        ```
+            if message is not None:
+                await self.server.send_chat(message)
 
-        :param event: The event to register the handler on.
-        :param listener: The listener to register.
+    async def _on_chat(self, uid: int, msg: str):
+        if msg.startswith(COMMAND_PREFIX):
+            logger.info('User %d sent command: %r', uid, msg)
+            try:
+                command = chat_command_factory(
+                    msg[len(COMMAND_PREFIX):]
+                )
+            except ValueError as e:
+                logger.warning(e)
+                await self.send_private_chat(uid, f'Invalid command: {msg}')
+            else:
+                # Wait a bit before executing the command to give the
+                # impression that the bot is typing the response.
+                await asyncio.sleep(0.2)
+                await command.execute(self, uid)
+
+    async def send_chat(self, message: str):
+        """Send a chat message to the server.
+
+        :param message: The message to send.
         """
-        return self.server.on(event, listener)
+        await self.server.send_chat(message)
 
-    def once(self, event: RoRClientEvents, listener: Callable | None = None):
-        """Decorator to register a one-time event handler on the event
-        emitter.
+    async def send_private_chat(self, uid: int, message: str):
+        """Send a private chat message to a user.
 
-        Example:
-        ```
-        @client.once(RoRClientEvents.CHAT)
-        def once_chat(unique_id: int, message: str):
-            print(f'User {unique_id} sent a message: {message}')
-        ```
-
-        :param event: The event to register the handler on.
-        :param listener: The listener to register.
+        :param uid: The user's UID.
+        :param message: The message to send.
         """
-        return self.server.once(event, listener)
+        await self.server.send_private_chat(uid, message)
+
+    async def send_multiline_chat(self, message: str):
+        """Sends a multiline message to the game chat.
+
+        :param message: The message to send.
+        """
+        max_line_len = 100
+        if len(message) > max_line_len:
+            logger.debug('[CHAT] multiline_message=%r', message)
+
+            total_lines = math.ceil(len(message) / max_line_len)
+            for i in range(total_lines):
+                line = message[max_line_len*i:max_line_len*(i+1)]
+                if i > 0:
+                    line = f'| {line}'
+                await self.send_chat(line)
+        else:
+            await self.send_chat(message)
+
+    async def kick(self, uid: int, reason: str = 'No reason given'):
+        """Kicks a user from the server.
+
+        :param uid: The uid of the user to kick.
+        :param reason: The reason for kicking the user, defaults to
+        'No reason given'
+        """
+        await self.send_chat(f'!kick {uid} {reason}')
+
+    async def ban(self, uid: int, reason: str = 'No reason given'):
+        """Bans a user from the server.
+
+        :param uid: The uid of the user to ban.
+        :param reason: The reason for banning the user, defaults to
+        'No reason given'
+        """
+        await self.send_chat(f'!ban {uid} {reason}')
+
+    async def say(self, uid: int, message: str):
+        """Send a message as a user anonymously.
+
+        :param uid: The uid of the user to send the message to. If -1,
+        the message will be sent to everyone.
+        :param message: The message to send.
+        """
+        await self.send_chat(f'!say {uid} {message}')
+
+    async def send_game_cmd(self, cmd: str):
+        """Send a game command to the server.
+
+        :param cmd: The command to send.
+        """
+        await self.server.send_game_cmd(cmd)
