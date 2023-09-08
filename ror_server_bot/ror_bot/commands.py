cf4bdf19b64932a03ccd805d4c393e8cab8f91eb
Danny Mackey <danmack768@gmail.com>
2023-09-08 17:38:46 -0400

    Added move/rotate RoRBot and stream recorder commands

--- ror_server_bot/ror_bot/commands.py
+++ ror_server_bot/ror_bot/commands.py
@@ -1,4 +1,7 @@
+import logging
+import math
 from enum import auto, StrEnum
+from pathlib import Path
 from typing import Annotated, Literal, TYPE_CHECKING
 
 from pydantic import BaseModel, Field, RootModel
@@ -6,10 +9,13 @@ from pydantic import BaseModel, Field, RootModel
 from ror_server_bot import __version__
 
 from .enums import AuthStatus, Color, RoRClientEvents
+from .models import Vector3
+from .stream_recorder import StreamRecordingError
 
 if TYPE_CHECKING:
     from .ror_client import RoRClient
 
+logger = logging.getLogger(__name__)
 
 COMMAND_PREFIX = '>'
 
@@ -24,6 +30,13 @@ class CommandType(StrEnum):
     GTG = auto()
     VERSION = auto()
     COUNTDOWN = auto()
+    MOVE_ROR_BOT = 'movebot'
+    ROTATE_ROR_BOT = 'rotatebot'
+    GET_POS = 'getpos'
+    GET_ROT = 'getrot'
+    RECORD = auto()
+    PLAYBACK = auto()
+    RECORDINGS = auto()
 
 
 class BaseChatCommand(BaseModel):
@@ -36,7 +49,7 @@ class BaseChatCommand(BaseModel):
 
     @property
     def usage(self) -> str:
-        return 'No usage provided.'
+        return f'{COMMAND_PREFIX}{self.name}'
 
     @property
     def name(self) -> str:
@@ -58,10 +71,6 @@ class PrefixCommand(BaseChatCommand):
     def description(self) -> str:
         return 'Get the prefix for commands.'
 
-    @property
-    def usage(self) -> str:
-        return f'{COMMAND_PREFIX}{self.name}'
-
     async def execute(self, client: 'RoRClient', uid: int):
         await client.send_chat(
             f'The prefix for commands is: {COMMAND_PREFIX}'
@@ -74,10 +83,6 @@ class PingCommand(BaseChatCommand):
     @property
     def description(self) -> str:
         return 'Ping the bot.'
-
-    @property
-    def usage(self) -> str:
-        return f'{COMMAND_PREFIX}{self.name}'
 
     async def execute(self, client: 'RoRClient', uid: int):
         await client.send_chat('pong')
@@ -92,7 +97,7 @@ class HelpCommand(BaseChatCommand):
 
     @property
     def usage(self) -> str:
-        return f'{COMMAND_PREFIX}{self.name} [command]'
+        return f'{super().usage} [command]'
 
     def no_args(self) -> str:
         commands = ', '.join(CommandType.__members__.values())
@@ -140,10 +145,6 @@ class SetStatusCommand(BaseChatCommand):
     def description(self) -> str:
         return f'Set your status to {self.name}.'
 
-    @property
-    def usage(self) -> str:
-        return f'{COMMAND_PREFIX}{self.name}'
-
     async def execute(self, client: 'RoRClient', uid: int):
         if len(self.args) == 0:
             username = client.server.get_username_colored(uid)
@@ -155,6 +156,8 @@ class SetStatusCommand(BaseChatCommand):
                 message = f'{username} is back!'
             elif self.command is CommandType.GTG:
                 message = f'{username} is gtg'
+            else:
+                raise ValueError(f'Invalid command {self.command}')
         else:
             message = 'Too many arguments'
 
@@ -167,10 +170,6 @@ class GetVersionCommand(BaseChatCommand):
     @property
     def description(self) -> str:
         return 'Get the version of the bot.'
-
-    @property
-    def usage(self) -> str:
-        return f'{COMMAND_PREFIX}{self.name}'
 
     async def execute(self, client: 'RoRClient', uid: int):
         await client.send_chat(f'RoR Server Bot v{__version__}')
@@ -185,7 +184,7 @@ class CountdownCommand(BaseChatCommand):
 
     @property
     def usage(self) -> str:
-        return f'{COMMAND_PREFIX}{self.name} <seconds>'
+        return f'{super().usage} <seconds>'
 
     async def execute(self, client: 'RoRClient', uid: int):
         if len(self.args) != 1:
@@ -230,6 +229,213 @@ class CountdownCommand(BaseChatCommand):
                     )
 
 
+class MoveRoRBotCommand(BaseChatCommand):
+    command: Literal[CommandType.MOVE_ROR_BOT]
+
+    @property
+    def description(self) -> str:
+        return 'Move the bot to a different position on the map.'
+
+    @property
+    def usage(self) -> str:
+        return f'{super().usage} <x> <y> <z>'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        if len(self.args) != 3:
+            await client.send_chat('Invalid number of arguments')
+            return
+
+        try:
+            new_pos = Vector3(x=self.args[0], y=self.args[1], z=self.args[2])
+        except Exception:
+            await client.send_chat('Invalid argument')
+            return
+
+        await client.move_bot(new_pos)
+        await client.send_chat(f'Moved bot to {new_pos}')
+
+
+class RotateRoRBotCommand(BaseChatCommand):
+    command: Literal[CommandType.ROTATE_ROR_BOT]
+
+    @property
+    def description(self) -> str:
+        return 'Rotate the bot a number of degrees.'
+
+    @property
+    def usage(self) -> str:
+        return f'{super().usage} <degrees>'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        if len(self.args) != 1:
+            await client.send_chat('Invalid number of arguments')
+            return
+
+        try:
+            rot_deg = float(self.args[0])
+        except ValueError:
+            await client.send_chat('Invalid argument')
+            return
+
+        rot_rad = math.radians(rot_deg)
+        await client.rotate_bot(rot_rad)
+        await client.send_chat(f'Rotated bot to {rot_deg}')
+
+
+class GetPositionCommand(BaseChatCommand):
+    command: Literal[CommandType.GET_POS]
+
+    @property
+    def description(self) -> str:
+        return 'Get your current position on the map.'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        position = client.server.get_position(uid)
+        await client.send_chat(f'Your position is {position:.2f}')
+
+
+class GetRotationCommand(BaseChatCommand):
+    command: Literal[CommandType.GET_ROT]
+
+    @property
+    def description(self) -> str:
+        return 'Get your current rotation on the map.'
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        rot_rad = client.server.get_rotation(uid)
+
+        if rot_rad is None:
+            await client.send_chat('Your rotation is unknown')
+            return
+
+        rot_deg = math.degrees(rot_rad)
+        await client.send_chat(f'Your rotation is {rot_deg:.2f}')
+
+
+class StreamRecorderSubcommand(StrEnum):
+    START = auto()
+    STOP = auto()
+    PLAY = auto()
+    PAUSE = auto()
+    RESUME = auto()
+
+
+class StreamRecorderCommand(BaseChatCommand):
+    command: Literal[
+        CommandType.RECORD,
+        CommandType.PLAYBACK,
+        CommandType.RECORDINGS
+    ]
+
+    @property
+    def description(self) -> str:
+        return 'Record or playback a stream.'
+
+    @property
+    def usage(self) -> str:
+        return super().usage
+
+    @property
+    def subcommand(self) -> StreamRecorderSubcommand:
+        return StreamRecorderSubcommand(self.args[0])
+
+    async def record(
+        self,
+        client: 'RoRClient',
+        uid: int,
+        sid: int
+    ):
+        match self.subcommand:
+            case (
+                StreamRecorderSubcommand.START | StreamRecorderSubcommand.PLAY
+            ):
+                user_info = client.server.get_user(uid).info
+                client.stream_recorder.start_recording(user_info, sid)
+                await client.send_chat(f'Recording {uid}:{sid}')
+            case StreamRecorderSubcommand.STOP:
+                client.stream_recorder.stop_recording(uid, sid)
+                await client.send_chat(f'Stopped recording {uid}:{sid}')
+            case StreamRecorderSubcommand.PAUSE:
+                client.stream_recorder.pause_recording(uid, sid)
+                await client.send_chat(f'Paused recording {uid}:{sid}')
+            case StreamRecorderSubcommand.RESUME:
+                client.stream_recorder.resume_recording(uid, sid)
+                await client.send_chat(f'Resumed recording {uid}:{sid}')
+            case _:
+                await client.send_chat('Invalid subcommand')
+
+    async def playback(self, client: 'RoRClient'):
+        if self.subcommand in (
+            StreamRecorderSubcommand.START, StreamRecorderSubcommand.PLAY
+        ):
+            if len(self.args) == 1:
+                filename = None
+            elif len(self.args) == 2:
+                filename = Path(self.args[1])
+            else:
+                await client.send_chat('Invalid number of arguments')
+                return
+            await client.stream_recorder.play_recording(filename)
+        else:
+            if len(self.args) == 1:
+                sid = None
+            elif len(self.args) == 2:
+                sid = int(self.args[1])
+            else:
+                await client.send_chat('Invalid number of arguments')
+                return
+            match self.subcommand:
+                case StreamRecorderSubcommand.STOP:
+                    await client.stream_recorder.stop_playback(sid)
+                case StreamRecorderSubcommand.PAUSE:
+                    client.stream_recorder.pause_playback(sid)
+                case StreamRecorderSubcommand.RESUME:
+                    client.stream_recorder.resume_playback(sid)
+                case _:
+                    await client.send_chat('Invalid subcommand')
+
+    async def recordings(self, client: 'RoRClient'):
+        if client.stream_recorder.available_recordings:
+            files = '\n'.join(
+                file.name for file in
+                client.stream_recorder.available_recordings
+            )
+            await client.send_chat(f'Available recordings:\n{files}')
+        else:
+            await client.send_chat('No recordings available')
+
+    async def execute(self, client: 'RoRClient', uid: int):
+        user = client.server.get_user(uid)
+        if user.auth_status not in AuthStatus.MOD | AuthStatus.ADMIN:
+            await client.send_chat('You do not have permission to do that.')
+            return
+
+        try:
+            if self.args:
+                if len(self.args) == 1:
+                    sid = client.server.get_user(uid).current_stream.stream_id
+                elif len(self.args) == 2:
+                    sid = int(self.args[1])
+                else:
+                    await client.send_chat('Invalid number of arguments')
+                    return
+
+                match self.command:
+                    case CommandType.RECORD:
+                        await self.record(client, uid, sid)
+                    case CommandType.PLAYBACK:
+                        await self.playback(client)
+                    case _:
+                        raise ValueError(f'Invalid command {self.command}')
+            elif self.command is CommandType.RECORDINGS:
+                await self.recordings(client)
+            else:
+                await client.send_chat('Invalid number of arguments')
+        except StreamRecordingError as e:
+            logger.error('[REC] %r', e, exc_info=True, stacklevel=2)
+            await client.send_chat(str(e))
+
+
 ChatCommandTypes = (
     PrefixCommand
     | PingCommand
@@ -237,6 +443,11 @@ ChatCommandTypes = (
     | SetStatusCommand
     | GetVersionCommand
     | CountdownCommand
+    | MoveRoRBotCommand
+    | RotateRoRBotCommand
+    | GetPositionCommand
+    | GetRotationCommand
+    | StreamRecorderCommand
 )
 
 ChatCommand = Annotated[ChatCommandTypes, Field(discriminator='command')]
